2db67348ccccd0313fefc12528e73ca9a4b40e9f
pollyjuice74 <hernandez.aht82836@gmail.com>
2024-05-02 12:02:26 -0400

    Latest

--- model.py
+++ model.py
@@ -1,34 +1,374 @@
 # Suppose 
 import torch
-import torch_geometric
 import torch.nn.functional as F
 import torch.nn as nn
+import time
 
+import torch_geometric
 from torch_geometric.nn.conv import GATv2Conv
 from torch_geometric.nn import TransformerConv, LSTMAggregation
 from torch_geometric.utils import to_dense_batch, sort_edge_index
 
+from utils import *
+
+from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.nn.dense import HeteroDictLinear, HeteroLinear
+from torch_geometric.nn.inits import ones
+from torch_geometric.nn.parameter_dict import ParameterDict
+from torch_geometric.typing import Adj, EdgeType, Metadata, NodeType
+from torch_geometric.utils import softmax
+from torch_geometric.utils.hetero import construct_bipartite_edge_index
+
+
 hidden_channels, num_classes = 0,3
+in_channels, out_channels = 0, 0
 
 
-class GAN(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, heads=1, concat=True, dropout=0.6, edge_dim=None):
-    
-        self.conv1 = GATv2Conv(
-            in_channels=in_channels, 
-            out_channels=out_channels, 
-            heads=heads, 
-            concat=concat, 
-            dropout=dropout,
-            edge_dim=edge_dim
-        )
-        #self.lstm_agg = LSTMAggregation()
+
+class Stock:
+    def __init__(self, sym, ammount, train=True):
+        self.price = self.get_price(sym) # setted with live_price
+        self.total = ammount * self.live_price(sym) if train else ammount * self.price(sym)
+
+        self.graph = build(sym)
+
+
+    def live_price(self, sym):
+        pass
+
+
+    def price(self, sym):
+        pass
+
+
+    def build(self, sym):
+        pass
+
+
+
+class HGTConv(MessagePassing):
+    def __init__(
+        self,
+        in_channels: Union[int, Dict[str, int]],
+        out_channels: int,
+        metadata: Metadata,
+        heads: int = 1,
+        **kwargs,
+    ):
+        super().__init__(aggr='add', node_dim=0, **kwargs)
+
+        if out_channels % heads != 0:
+            raise ValueError(f"'out_channels' (got {out_channels}) must be "
+                             f"divisible by the number of heads (got {heads})")
+
+        if not isinstance(in_channels, dict):
+            in_channels = {node_type: in_channels for node_type in metadata[0]}
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.heads = heads
+        self.node_types = metadata[0]
+        self.edge_types = metadata[1]
+        self.edge_types_map = {
+            edge_type: i
+            for i, edge_type in enumerate(metadata[1])
+        }
+
+        self.dst_node_types = set([key[-1] for key in self.edge_types])
+
+        self.kqv_lin = HeteroDictLinear(self.in_channels,
+                                        self.out_channels * 3)
+
+        self.out_lin = HeteroDictLinear(self.out_channels, self.out_channels,
+                                        types=self.node_types)
+
+        dim = out_channels // heads
+        num_types = heads * len(self.edge_types)
+
+        self.k_rel = HeteroLinear(dim, dim, num_types, bias=False,
+                                  is_sorted=True)
+        self.v_rel = HeteroLinear(dim, dim, num_types, bias=False,
+                                  is_sorted=True)
+
+        self.skip = ParameterDict({
+            node_type: Parameter(torch.empty(1))
+            for node_type in self.node_types
+        })
+
+        self.p_rel = ParameterDict()
+        for edge_type in self.edge_types:
+            edge_type = '__'.join(edge_type)
+            self.p_rel[edge_type] = Parameter(torch.empty(1, heads))
+
+        self.reset_parameters()
+
+
+    def reset_parameters(self):
+        super().reset_parameters()
+        self.kqv_lin.reset_parameters()
+        self.out_lin.reset_parameters()
+        self.k_rel.reset_parameters()
+        self.v_rel.reset_parameters()
+        ones(self.skip)
+        ones(self.p_rel)
+
+
+    def _cat(self, x_dict: Dict[str, Tensor]) -> Tuple[Tensor, Dict[str, int]]:
+        """Concatenates a dictionary of features."""
+        cumsum = 0
+        outs: List[Tensor] = []
+        offset: Dict[str, int] = {}
+        for key, x in x_dict.items():
+            outs.append(x)
+            offset[key] = cumsum
+            cumsum += x.size(0)
+        return torch.cat(outs, dim=0), offset
+
+
+    def _construct_src_node_feat(
+        self, k_dict: Dict[str, Tensor], v_dict: Dict[str, Tensor],
+        edge_index_dict: Dict[EdgeType, Adj]
+    ) -> Tuple[Tensor, Tensor, Dict[EdgeType, int]]:
+        """Constructs the source node representations."""
+        cumsum = 0
+        num_edge_types = len(self.edge_types)
+        H, D = self.heads, self.out_channels // self.heads
+
+        # Flatten into a single tensor with shape [num_edge_types * heads, D]:
+        ks: List[Tensor] = []
+        vs: List[Tensor] = []
+        type_list: List[Tensor] = []
+        offset: Dict[EdgeType] = {}
+        for edge_type in edge_index_dict.keys():
+            src = edge_type[0]
+            N = k_dict[src].size(0)
+            offset[edge_type] = cumsum
+            cumsum += N
+
+            # construct type_vec for curr edge_type with shape [H, D]
+            edge_type_offset = self.edge_types_map[edge_type]
+            type_vec = torch.arange(H, dtype=torch.long).view(-1, 1).repeat(
+                1, N) * num_edge_types + edge_type_offset
+
+            type_list.append(type_vec)
+            ks.append(k_dict[src])
+            vs.append(v_dict[src])
+
+        ks = torch.cat(ks, dim=0).transpose(0, 1).reshape(-1, D)
+        vs = torch.cat(vs, dim=0).transpose(0, 1).reshape(-1, D)
+        type_vec = torch.cat(type_list, dim=1).flatten()
+
+        k = self.k_rel(ks, type_vec).view(H, -1, D).transpose(0, 1)
+        v = self.v_rel(vs, type_vec).view(H, -1, D).transpose(0, 1)
+
+        return k, v, offset
+
+
+    def forward(
+        self,
+        x_dict: Dict[NodeType, Tensor],
+        edge_index_dict: Dict[EdgeType, Adj]  # Support both.
+    ) -> Dict[NodeType, Optional[Tensor]]:
+        F = self.out_channels
+        H = self.heads
+        D = F // H
+
+        k_dict, q_dict, v_dict, out_dict = {}, {}, {}, {}
+
+        # Compute K, Q, V over node types:
+        kqv_dict = self.kqv_lin(x_dict)
+        print(kqv_dict)
+        for key, val in kqv_dict.items():
+            k, q, v = torch.tensor_split(val, 3, dim=1)
+            k_dict[key] = k.view(-1, H, D)
+            q_dict[key] = q.view(-1, H, D)
+            v_dict[key] = v.view(-1, H, D)
+
+        q, dst_offset = self._cat(q_dict)
+        k, v, src_offset = self._construct_src_node_feat(
+            k_dict, v_dict, edge_index_dict)
+
+        edge_index, edge_attr = construct_bipartite_edge_index(
+            edge_index_dict, src_offset, dst_offset, edge_attr_dict=self.p_rel,
+            num_nodes=k.size(0))
+
+        print(edge_index.shape, k.permute(2, 1, 0).shape, q.shape, v.permute(2, 1, 0).shape, edge_attr.shape)
+        print(edge_index)
+        out = self.propagate(edge_index, k=k.permute(2, 1, 0), q=q, v=v.permute(2, 1, 0), edge_attr=edge_attr)
+
+        # Reconstruct output node embeddings dict:
+        for node_type, start_offset in dst_offset.items():
+            end_offset = start_offset + q_dict[node_type].size(0)
+            if node_type in self.dst_node_types:
+                out_dict[node_type] = out[start_offset:end_offset]
+
+        # Transform output node embeddings:
+        a_dict = self.out_lin({
+            k:
+            torch.nn.functional.gelu(v) if v is not None else v
+            for k, v in out_dict.items()
+        })
+
+        # Iterate over node types:
+        for node_type, out in out_dict.items():
+            out = a_dict[node_type]
+
+            if out.size(-1) == x_dict[node_type].size(-1):
+                alpha = self.skip[node_type].sigmoid()
+                out = alpha * out + (1 - alpha) * x_dict[node_type]
+            out_dict[node_type] = out
+
+        return out_dict
+
+
+    def message(self, k_j: Tensor, q_i: Tensor, v_j: Tensor, edge_attr: Tensor,
+                index: Tensor, ptr: Optional[Tensor],
+                size_i: Optional[int]) -> Tensor:
+        alpha = (q_i * k_j).sum(dim=-1) * edge_attr
+        alpha = alpha / math.sqrt(q_i.size(-1))
+        alpha = softmax(alpha, index, ptr, size_i)
+        out = v_j * alpha.view(-1, self.heads, 1)
+        return out.view(-1, self.out_channels)
+
+
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}(-1, {self.out_channels}, '
+                f'heads={self.heads})')
+
+
+
+class HGT(torch.nn.Module):
+    def __init__(self, metadata, hidden_channels, num_classes=3, heads=1, num_layers=1):
+        super().__init__()
+
+        # node_type: linear layer
+        self.lin_dict = torch.nn.ModuleDict()
+
+        # List of HGTConv layers
+        self.convs = torch.nn.ModuleList()
+
+        for _ in range(num_layers):
+          conv = HGTConv(
+              in_channels=hidden_channels,
+              out_channels=hidden_channels,
+              metadata=metadata,
+              heads=heads,
+          )
+          self.convs.append(conv)
+
         self.classifier = nn.Linear(hidden_channels, num_classes)
 
-    def forward(self, x, edge_index, edge_attr=None):
-        x = self.conv1(x, edge_index)
-        x = F.relu(x)
-        x = F.dropout(x, training=self.training)
 
-        out = self.classifier(x)
-        return out
+    def forward(self, x_dict, edge_index_dict, edge_attr=None):
+        # print([v.shape for k, v in x_dict.items()])
+        # print(edge_index_dict)
+
+        for node_type, x in x_dict.items():
+            out_channels = x.size(0)
+            layer = Linear(out_channels, 245)
+            x_dict[node_type] = layer(x.T).relu()
+
+        print([v.shape for k, v in x_dict.items()])
+        #print(x_dict)
+
+        for conv in self.convs:
+            x_dict = conv(x_dict, edge_index_dict)
+
+        return self.classifier(x_dict)
+
+
+
+class Account:
+    def __init__(self, hidden_channels=7*35, epochs=2000, learning_rate=1e-05):
+        # Account info
+        self.net_value = 416.29
+        self.symbols = ['AMZN',] #"TSLA", "AAPL", "GOOGL", "META", "GM", "MS"]
+        self.bets = 100 # Limited ammount of bets per day
+        # Bets
+        self.current_bets = {sym: 0 for sym in self.symbols}
+        self.stocks = [] # Graphs of stocks
+        # Data
+        self.graph = build_graph(self.symbols)
+        # Model
+        self.model = HGT(metadata=self.graph.metadata(), hidden_channels=hidden_channels)
+        # Training
+        self.epochs = epochs
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=learning_rate)
+        self.criterion = nn.BCEWithLogitsLoss()
+        ##################################################
+
+
+    def train(self):
+        for i in range(self.epochs):
+            epoch_loss = 0
+            for sym in self.symbols:
+                print("\nTraining on: ", sym)
+
+                for _ in range(1000): # sample 1000 times
+                    sample_graph, ix = sample(self.graph, sym) # Samples Training graph nodes
+                    pct = torch.std(sample_graph[sym].x_samp) * 0.5 # try to predict a half std movement
+                    print("Percent pred: ", pct.item())
+
+                    while sample_graph[sym].x_pred.numel() > 0: # while there is more data to predict
+
+                        y_hat = self.model(sample_graph.x_samp_dict, sample_graph.edge_index_dict)
+                        y = movement(sample_graph[sym].x_pred[0], pct=pct.item())
+
+                        loss = self.criterion(y_hat, y)
+                        step(sample_graph, ix, sym)
+
+                        self.optimizer.zero_grad(set_to_none=True)
+                        loss.backward()
+                        self.optimizer.step()
+
+                epoch_loss += loss.item()
+
+                self.print(i, epoch_loss, freq=25)
+
+
+
+
+    def test(self, graph, live=False): # Live trading simulation
+        for sym in self.symbols:
+            x, edge_index = sample_graph_live(graph, sym) if live else sample_graph(graph, sym)# Samples LIVE graph nodes
+            pred = self.model(x, edge_index, pct=0.03)
+
+            if self.buy_conditions(pred):
+                ammount = self.get_portion()
+                self.buy(sym, ammount)
+        
+        self.wait()
+        self.sell(sym, ammount)
+
+
+    def get_portion(self):
+        pass
+
+
+    def buy_conditions(self):
+        pass
+
+
+    def print(self, i, epoch_loss, freq=25):
+        if i % freq == 0:
+            print(f"Epoch: {i+1}/{self.epochs}, Loss: {epoch_loss}")
+
+
+    def buy(self, sym, ammount):
+        self.current_bets[sym] += ammount # sym: ammount of stocks owned
+
+        stock = Stock(sym, ammount)
+        self.net_value -= ammount * stock.price
+        
+        self.stocks.append(stock)
+
+
+    def wait(self):
+        time.sleep(10)
+
+
+    def sell(self, stock, sym, ammount):
+        self.current_bets[sym] -= ammount # sym: ammount of stocks owned
+        self.net_value += ammount * stock.price
+        
+        self.stocks.remove(stock)
--- utils.py
+++ utils.py
@@ -1,28 +1,195 @@
-from torch_geometric.data import TemporalData, Dataset
-import pandas as pd
+from torch_geometric.data import HeteroData
 import yfinance as yf
 import torch
-
-from data import *
-
-# response = fetch(url)
-# response. # get symbols
-# stocks = ["SPY", "AMZN", "TSLA", "AAPL", "GOOGL", "META", "GM", "MS"]
-# market = get_portfolio("SPY")
+import datetime as dt
 
 
-def train():
-    # get model
-    # get stock graph, sample graph
-    # loop:
-        # predict y, step, until len(x_pred)==0
-        # backprop
-    pass
+
+def sym_graph(symbol, interval='1h'):
+  graph = HeteroData()
+  df = yf.download(symbol, start=startDate, end=endDate, interval=interval)
+
+  # Data
+  msg_features = ['High', 'Low', 'Close', 'Open']
+  msg = torch.tensor(df[msg_features][:-1].values, dtype=torch.float)
+  t = df.index.strftime('%H%M%S').astype('int64')
+  node_ids = torch.tensor(df.index.strftime('%Y%m%d%H%M%S').astype('int64'), dtype=torch.int64)
+  #print(node_ids, msg, t)
+
+  # Add data to graph
+  graph.sym = symbol
+  graph[symbol].x = msg
+  graph[symbol].t = t
+  graph[symbol].node_ids = node_ids
+
+  # Create edges based on time sequence
+  num_nodes = msg.size(0)
+  src = torch.arange(0, num_nodes - 1, dtype=torch.long)  # Indices from 0 to N-2
+  dst = torch.arange(1, num_nodes, dtype=torch.long)
+  graph[symbol, 'next_in_sequence', symbol].edge_index = torch.stack([src, dst], dim=0)
+
+  return graph
+
+
+def build_graph(stock_symbols):
+  graph = HeteroData()
+  sp = sym_graph('SPY')
+  stock_graphs = [sym_graph(sym) for sym in stock_symbols]
+
+  # Add stock graphs to graph
+  for s_graph in stock_graphs:
+    graph[s_graph.sym].x = s_graph[s_graph.sym].x
+    graph[s_graph.sym].t = s_graph[s_graph.sym].t
+    graph[s_graph.sym].node_ids = s_graph[s_graph.sym].node_ids
+    graph[s_graph.sym, 'next_in_sequence', s_graph.sym].edge_index = s_graph[s_graph.sym, 'next_in_sequence', s_graph.sym].edge_index
+
+  # Add SPY stock graph
+  graph[sp.sym].x = sp[sp.sym].x
+  graph[sp.sym].t = sp[sp.sym].t
+  graph[sp.sym].node_ids = sp[sp.sym].node_ids
+  graph[sp.sym, 'next_in_sequence', sp.sym].edge_index = sp[sp.sym, 'next_in_sequence', sp.sym].edge_index
+
+  # Link all symbol nodes to SPY nodes at the 'same_time' t
+  sp_nodes = graph[sp.sym].node_ids
+  for sym in stock_symbols:
+    stock_nodes = graph[sym].node_ids
+    graph[sp.sym, 'same_time', sym].edge_index = link_graphs(sp_nodes, stock_nodes)
+
+  return graph
+
+
+def link_graphs(sp_nodes, stock_nodes):
+  # SPY/Stock look up node_id: idx
+  stock_mapping = {node_id.item(): idx for idx, node_id in enumerate(stock_nodes)}
+  sp_mapping = {node_id.item(): idx for idx, node_id in enumerate(sp_nodes)}
+
+  common_node_ids = set(stock_mapping.keys()).intersection(sp_mapping.keys())
+
+  # Makes list of common node id's idxs
+  stock_indices = torch.tensor(sorted([stock_mapping[nid] for nid in common_node_ids]), dtype=torch.long) ###
+  sp_indices = torch.tensor(sorted([sp_mapping[nid] for nid in common_node_ids]), dtype=torch.long) ###
+
+  return torch.stack([sp_indices, stock_indices], dim=0)
+
+
+#######################################################################
+def sample(graph, sym, sample_len=7*30, pred_len=7*5):
+  sample_graph = HeteroData()
+
+  # random stock sample index
+  s_ix = torch.randint(0, graph[sym].x.size(0) - sample_len - pred_len, (1,))
+  curr_ix = s_ix+sample_len
+
+  # stock graph
+  sample_graph[sym].x = graph[sym].x[s_ix:curr_ix + pred_len]
+  sample_graph[sym].t = graph[sym].t[s_ix:curr_ix + pred_len]
+  sample_graph[sym].node_ids = graph[sym].node_ids[s_ix:curr_ix + pred_len]
+
+  # stock nodesIDs
+  first_nodeID = graph[sym].node_ids[s_ix]
+  curr_nodeID = graph[sym].node_ids[curr_ix]
+  last_nodeID = graph[sym].node_ids[curr_ix + pred_len]
+
+  # spy nodes ixs
+  f_sp_ix = torch.where(graph['SPY'].node_ids == first_nodeID.item())[0]
+  c_sp_ix = torch.where(graph['SPY'].node_ids == curr_nodeID.item())[0]
+  l_sp_ix = torch.where(graph['SPY'].node_ids == last_nodeID.item())[0]
+
+  # spy graph
+  sample_graph['SPY'].x = graph['SPY'].x[f_sp_ix:l_sp_ix]
+  sample_graph['SPY'].t = graph['SPY'].t[f_sp_ix:l_sp_ix]
+  sample_graph['SPY'].node_ids = graph['SPY'].node_ids[f_sp_ix:l_sp_ix]
+
+  # sample edges
+  sym_edges = graph[sym, 'next_in_sequence', sym].edge_index[:, s_ix:curr_ix + pred_len] ###
+  spy_edges = graph['SPY', 'next_in_sequence', 'SPY'].edge_index[:, f_sp_ix:l_sp_ix] ###
+  same_time_edges =  graph['SPY', 'same_time', sym].edge_index[:, s_ix:curr_ix + pred_len -1] ###
+
+  dicts, edge_ixs = make_dicts([spy_edges, sym_edges]) # spy, sym
+  same_time_edges = same_time_ix(same_time_edges, dicts) # convert same time ixs from graph ixs to sample graph ixs
+
+  # set edges
+  sample_graph['SPY', 'next_in_sequence', 'SPY'].edge_index = edge_ixs[0]
+  sample_graph[sym, 'next_in_sequence', sym].edge_index = edge_ixs[1]
+  sample_graph['SPY', 'same_time', sym].edge_index = same_time_edges
+
+  # normalize stock data
+  sample_graph[sym].x_samp, sample_graph[sym].x_pred = normalize(sample_graph[sym].x, curr_ix=curr_ix - s_ix, pred_ix=curr_ix+pred_len - s_ix) # shifts ixs by first stock ix
+  sample_graph['SPY'].x_samp, sample_graph['SPY'].x_pred = normalize(sample_graph['SPY'].x, curr_ix=c_sp_ix - f_sp_ix, pred_ix=l_sp_ix - f_sp_ix) # shifts ixs by first spy stock ix
+
+  # EDGE_INDEX_SAMP, EDGE_INDEX_PRED
+  #
+  #
+
+  # REMOVE REDUNDANT sample_graph.x #
+  del sample_graph[sym].x
+  del sample_graph['SPY'].x
+
+  return sample_graph, len(sample_graph[sym].x_samp)
+#######################################################################
+
+
+def make_dicts(edge_sets):
+  dicts = list()
+  edge_ixs = list()
+
+  for edge_samp in edge_sets:
+    num_nodes = edge_samp.shape[1]
+
+    edge_ix = torch.stack([torch.arange(0, num_nodes, dtype=torch.long),    # [[0, 1, 2, ...],
+                          torch.arange(1, num_nodes+1, dtype=torch.long)], dim=0) #  [1, 2, 3, ...]]
+
+    # dict of graph ixs to sample graph ixs
+    gix_to_sgix = {edge_samp[j, i].item(): edge_ix[j, i].item()
+                    for i in range(num_nodes) for j in range(2)} # [2, num_nodes]
+    # gix_to_sgix[edge_samp[1, num_nodes-2].item()] = edge_ix[1, num_nodes-2].item()
+
+    dicts.append(gix_to_sgix)
+    edge_ixs.append(edge_ix)
+
+  return dicts, edge_ixs # spy, sym
+
+
+def same_time_ix(same_time, dicts):
+  edge_ix = torch.zeros_like(same_time) # same shape
+
+  for i in range(same_time.shape[1]):
+    edge_ix[0, i] = dicts[0][same_time[0, i].item()] # spy dict
+    edge_ix[1, i] = dicts[1][same_time[1, i].item()] # sym dict
+
+  return edge_ix
+
+
+def step(sample_graph, ix, sym):
+  sp_ix_raw, _ = sample_graph['SPY', 'same_time', sym][:, ix]
+
+  sample_graph[sym].x_samp = torch.cat([sample_graph[sym].x_samp, sample_graph[sym].x_pred[:1]])
+  sample_graph[sym].x_pred = sample_graph[sym].x_pred[1:]
+
+  sp_edge_indices = sample_graph['SPY', 'next_in_sequence', 'SPY'].edge_index[1]
+  sp_ix = torch.where(sp_edge_indices == sp_ix_raw)[0].item()
+  print(len(sp_edge_indices), sp_ix_raw, sp_ix)
+
+  sample_graph['SPY'].x_samp = sample_graph['SPY'].x[:sp_ix]
+  sample_graph['SPY'].x_pred = sample_graph['SPY'].x[sp_ix:]
+
+  return ix+1
+
+
+def normalize(x, curr_ix, pred_ix):
+  x_samp = x[:curr_ix]
+  last_close = float(x_samp[-1, 2]) # Scale by last Close price
+
+  x_samp /= last_close
+
+  x_pred = x[curr_ix:pred_ix+1]
+  x_pred /= last_close
+  return x_samp, x_pred
 
 
 def movement(x_pred, pct=0.03):
-    high = max(x_pred[:,0])
-    low = min(x_pred[:,1])
+    high = max(x_pred[0])
+    low = min(x_pred[1])
 
     if high >= 1 + pct:
         y = torch.tensor([1, 0, 0])  # above Pct return
@@ -33,12 +200,6 @@ def movement(x_pred, pct=0.03):
 
     return y
 
-
-def step(x_samp, x_pred):
-    # Adds
-    x_samp = torch.cat([x_samp, x_pred[:1]])
-    x_pred = x_pred[1:]
-    return x_samp, x_pred
 
 
 def get_symbols(symbols):
