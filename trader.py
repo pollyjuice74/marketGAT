97427f0e6d2e0a961097ba676e22067bc6b93fe3
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-30 23:24:42 -0400

    Update trader.py

--- trader.py
+++ trader.py
@@ -1,3 +1,24 @@
-class Trader():
-    def __init__(self, account):
-        self.account = account if account not None else Account()
+from acc import Account
+
+
+class LIVETrader(Account):
+    def __init__(self,):
+        self.login_url = ""
+        super( balance=self.live_balance() ).init()
+
+    def call(self):
+        pass
+
+    def live_price(self, sym):
+        return price
+
+    def live_balance(self)
+        return balance
+
+    def execute_buy(self, sym):
+        pass
+        
+    def execute_sell(self, sym):
+        pass
+
+    def 
